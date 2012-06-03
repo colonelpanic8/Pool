d7b4412abaeb26ed8904bd76e2fb9c5a84fa7d1f
Ivan Malison <IvanMalison@gmail.com>
2012-06-02 21:34:36 -0700

    Made a main class. Didn't make sense having the PoolFrame also be runnable.

--- src/pool/PoolFrame.java
+++ src/pool/PoolFrame.java
@@ -1,10 +1,10 @@
 package pool;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -142,79 +142,5 @@ public class PoolFrame {
 	    }
 	    msp.repaint();
 	}
-    }
-    public static void main(String[] args) {
-	JFrame window = new JFrame("Pool");
-	PoolPanel poolpanel = new PoolPanel(.3,.35, 25, 13);
-	JPanel content = new JPanel();
-	JPanel south = new JPanel();
-	
-	JComboBox colorChoice = new JComboBox();
-	colorChoice.addItem("Red");
-	colorChoice.addItem("Blue");
-	colorChoice.addItem("Green");
-	colorChoice.addItem("Yellow");
-	
-	JSlider angle = new JSlider(0,5000,0);//Defines angle of shot
-	AngleListener alist = new AngleListener(poolpanel);
-	angle.addChangeListener(alist);
-	
-	JButton button = new JButton("New Ball");
-	NBControl nbcontrol = new NBControl(poolpanel, colorChoice);
-	button.addActionListener(nbcontrol);
-	
-	JButton selMode = new JButton("Selection Mode");
-	ModeListener mode = new ModeListener(poolpanel);
-	selMode.addActionListener(mode);
-        
-        JButton powerMode = new JButton("Slider Mode");
-	ShootingModeListener shootingMode = new ShootingModeListener(poolpanel);
-	powerMode.addActionListener(shootingMode);
-	
-	JButton shoot = new JButton("Shoot");
-	SListener slistener = new SListener(poolpanel);
-	shoot.addActionListener(slistener);
-        
-        JButton makeRack = new JButton("Make Rack");
-	MakeRackListener rackListener = new MakeRackListener(poolpanel);
-	makeRack.addActionListener(rackListener);
-	
-	JSlider power = new JSlider(0,100,50);
-	PowerListener plist = new PowerListener(poolpanel, slistener);
-	power.addChangeListener(plist);
-	power.setOrientation(JSlider.VERTICAL);
-        
-        JSlider spin = new JSlider(-25,25,0);
-        SpinListener spinListener = new SpinListener(poolpanel);
-        spin.addChangeListener(spinListener);
-        
-        JButton snap = new JButton("Snap");
-	SnapListener snapListener = new SnapListener(poolpanel);
-	snap.addActionListener(snapListener);
-        
-        
-                
-	
-	
-	
-	south.setBackground(Color.GRAY);
-	content.setLayout(new BorderLayout());
-	content.add(poolpanel, BorderLayout.CENTER);
-	content.add(angle, BorderLayout.NORTH);
-	content.add(power, BorderLayout.EAST);
-        south.add(snap);
-        south.add(makeRack);
-        south.add(powerMode);
-	south.add(selMode);
-	south.add(button);
-	south.add(shoot);
-	south.add(colorChoice);
-        south.add(spin);
-	content.add(south, BorderLayout.SOUTH);
-	window.setContentPane(content);
-	window.setSize(1200, 700);
-	window.setLocation(100,100);
-	window.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-	window.setVisible(true);
-    }
+    }   
 }
